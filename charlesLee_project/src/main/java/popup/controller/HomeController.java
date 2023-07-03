56b1f56ff226d9dfd58668e5b12b7090bdf2b2aa
chokimi2000 <chokimi2000@naver.com>
2023-07-03 11:43:53 +0900

    no message

--- charlesLee_project/src/main/java/popup/controller/HomeController.java
+++ charlesLee_project/src/main/java/popup/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/home")
 	public String home() {
-		System.out.println("home test");
+		System.out.println("home test1");
 		return "home";
 	}
 	
