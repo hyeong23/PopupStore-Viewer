8610e4f4e5659833ab8605f45686b639f7259e2a
Hong <honghy97@naver.com>
2023-07-19 12:57:24 +0900

    마이페이지- 좋아요 리스트 페이지 화면 수정중

--- charlesLee_project/src/main/java/popup/controller/MyPageController.java
+++ charlesLee_project/src/main/java/popup/controller/MyPageController.java
@@ -3,6 +3,9 @@ package popup.controller;
 
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpSession;
 
@@ -10,22 +13,32 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import popup.dto.Member;
+import popup.dto.Store;
 import popup.service.MemberService;
+import popup.service.StoreService;
 
 @Controller
 public class MyPageController {
 	
 	@Autowired
 	MemberService memberService;
+	
+	@Autowired
+	StoreService storeService;
 	
 	@RequestMapping(value = "/mypage", method = RequestMethod.GET)
 	public String viewMyPage(Model model,HttpSession session) throws Exception {
@@ -181,11 +194,57 @@ public class MyPageController {
     }
 	
 	// 좋아요 리스트 
-	@RequestMapping(value = "/heart", method = RequestMethod.GET)
-	public String viewHeart(Model model,HttpSession session) throws Exception {
-	    
-	    return "heart";
-	}
+		@RequestMapping(value = "/heart", method = RequestMethod.GET)
+		public String viewHeart(Model model,HttpSession session) throws Exception {
+			List<Map<String, String>> member = storeService.getStore();
+			model.addAttribute("member", member);
+//			System.out.println(member);
+			for(int i = 0; i < member.size(); i++){
+				System.out.println("list 순서 " + i + "번쨰"); 
+		        for(Entry<String, String> elem : member.get(i).entrySet() ){ 
+		        	 System.out.println( String.format("키 : %s, 값 : %s", elem.getKey(), elem.getValue()) );
+		        }
+		            //System.out.println(key + " : " + value);// json객체 
+		        //});
+			}
+
+			return "heart";
+		}
+		
+		@RequestMapping(value = "/heart", method = RequestMethod.POST)
+		public String updateStore(@ModelAttribute Store store,
+								  @Param("category1") String category1,
+								  @Param("category2") String category2,
+								  @RequestPart("picture") MultipartFile[] pictures,
+								  HttpSession session,
+								  Model model)  {
+				String view = "error";
+					
+					boolean storeResult = false;
+					boolean categoryResult1 = false;
+					boolean categoryResult2 = false;
+					boolean pictureResult = false;
+					
+					int storeNum = 0;
+					
+					try {
+						
+						if(category2 != null) {
+						
+						
+						}else {
+							
+							System.out.println(category1 + " " + category2);
+						}
+						
+						
+					} catch (Exception e) {
+						e.printStackTrace();
+						return view;
+					}
+			
+			return "heart";
+		} 
 	
 }
 
